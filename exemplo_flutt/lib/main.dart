4b96a5ed2f2bf3b48311db24a205e3aa80ac61d6
Carlos Pelegrin <carloseduardopelegrin@gmail.com>
2021-06-15 10:38:26 -0300

    Transformando o Layout em página. Unidade 4 Aula 5

--- exemplo_flutt/lib/main.dart
+++ exemplo_flutt/lib/main.dart
@@ -13,28 +13,38 @@ class MyApp extends StatelessWidget {
           primaryColor: Colors.green,
           primaryColorDark: Colors.green[900],
           accentColor: Colors.amber[900]),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Título"),
-        ),
-        body: Container(
-          color: Colors.white,
-          child: Center(
-            child: Text(
-              "Hello World!",
-              style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.bold,
-              ),
+      home: HomePage(),
+    );
+  }
+}
+
+//Transformando o layout em página para depois retirar do arquivo
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Título"),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Text(
+            "Hello World!",
+            style: TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.bold,
             ),
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print("Funciona o Click");
-          },
-          child: Icon(Icons.import_contacts),
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("Funciona o Click");
+        },
+        child: Icon(Icons.import_contacts),
       ),
     );
   }
